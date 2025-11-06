064063c3f7272e500d8c03d9d4593e68d6f44fc0
irodriguezcardenas@byupathway.edu <irodriguezcardenas@byupathway.edu>
2025-11-06 18:43:00 -0500

    Changes to my assingments

---
+++ week02/script.js
@@ -0,0 +1,38 @@
+const DAYS = 6;
+const LIMIT = 30;
+let studentReport = [11, 42, 33, 64, 29, 37, 44];
+
+for (let i=0; i<studentReport.length; i++) {
+    if (studentReport[i] < LIMIT){
+        console.log(studentReport[i]);
+    }
+}
+
+let i = 0;
+while(i < studentReport.length) {
+    if (studentReport[i] < LIMIT){
+        console.log(studentReport[i]);
+    }
+    i++;
+}
+
+studentReport.forEach(function (item){
+    if (item < LIMIT){
+        console.log(item);
+    }
+});
+
+for (let i in studentReport) {
+    if (studentReport[i] < LIMIT) {
+      console.log(studentReport[i]);
+    }
+  }
+
+  const today = new Date();
+
+  for (let i=0; i < DAYS; i++){
+    let nextDay = new Date(today);
+    nextDay.setDay(today.getDate() + i);
+    let dateName = nextDay.toLocaleDateString("en-US", { weekday: "long" });
+    console.log(dayName);
+  }
